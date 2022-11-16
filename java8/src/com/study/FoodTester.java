e2f5db26f3e97229ea90a7c923f38600fde171be
programmingstudento <utsabkarkee1@gmail.com>
2022-11-16 12:48:50 +0545

    Vowel Count.

--- java8/src/com/study/FoodTester.java
+++ java8/src/com/study/FoodTester.java
@@ -11,6 +11,14 @@ public class FoodTester {
 				.sorted((o1, o2) -> Integer.compare(o2.getCalories(), o1.getCalories())).limit(3)
 				.map(food -> food.getName().concat(" " + food.getCalories())).collect(Collectors.toList());
 		System.out.println(foods);
+		System.out.println("-------------------------------");
+		Food.foods().stream().sorted((o1, o2) -> Integer.compare(o2.getCalories(), o1.getCalories())).map(food -> {
+			System.out.println(food);
+			return food.getName().concat(" " + food.getCalories());
+		}).limit(3).forEach(System.out::println);
 
+		int vowelCount = Food.foods().stream().map(Food::getName)
+				.mapToInt(food -> food.replaceAll("[^aeiouAEIOU]", "").length()).sum();
+		System.out.println(vowelCount);
 	}
 }
