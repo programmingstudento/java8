145805259435e62d2ff90f064e918c6206692614
programmingstudento <utsabkarkee1@gmail.com>
2022-12-04 15:00:45 +0545

    Hw.

--- java8/src/com/study/One.java
+++ java8/src/com/study/One.java
@@ -1,10 +1,29 @@
 package com.study;
 
+import static java.lang.Math.pow;
+
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
 public class One {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	}
+
+	public static long ipsBetween(String start, String end) {
+		final int NUMBER = 256;
+		var one = start.split("\\.");
+		var two = end.split("\\.");
+		int size = one.length;
+		return LongStream.range(0, size).map(index -> (int) Math.pow(NUMBER, size - 1 - index)
+				* (Long.valueOf(two[(int) index]) - Long.valueOf(one[(int) index]))).sum();
+
+	}
+
+	public static double solution(int[] arr1, int[] arr2) {
+		return IntStream.range(0, arr1.length).mapToDouble(index -> pow(arr2[index] - arr1[index], 2d)).sum()
+				/ arr1.length;
 	}
 
 }
